59675e0542cfcca87e256adec520ff62ffd55109
Hansraj100 <89459145+Hansraj100@users.noreply.github.com>
2023-10-02 19:17:32 +0000

    conversions and its basics

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,5 +17,45 @@ let booleanIsLoggedIn = Boolean( isLoggedIn)
 // ""=>false,"hansraj"=>true
 let someNumber = 33
 let StringNumber = String(someNumber)
-console.log(StringNumber );
-console.log(typeof StringNumber);
+// console.log(StringNumber );
+// console.log(typeof StringNumber);
+//*********0PERATIONS*************/
+let value = 3
+let negValue = -value
+// console.log(negValue);
+// console.log(2+2)
+// console.log(2-2)
+//console.log(2*2)
+// console.log(2**2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+//THIS IS HOW STRINGS CAN BE CONCATINATE
+let str1 = "hansraj"
+let str2 = " panwar"
+let str3 = str1 + str2
+// console.log(str3);
+// THIS IS WHERE THE PROBLEM ARISES
+// console.log( "1" + 2);
+// console.log( 1 + "2");
+// console.log( "1" + 2 +2);
+//IN THIS WE SEE THAT , THE FIRST TYPE IS USED IN WHOLE AND IT IS TREATED AS SAME
+// console.log( 1 + 2 + "2")//ANSWER=32
+//WE ARE ADVISED TO USE PARENTHESIS FOR GOOD PROGRAMMING OF JAVASCRIPT
+// console.log( "1" + 2);
+// console.log( 1 + "2");
+// console.log( "1" + 2 +2);
+//console.log((2 + 4) * 4 % 5);
+// console.log(true);
+// console.log(+true);//answer= 1
+// console.log(true+);//answer=expected error
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2;
+
+//SMALL OPERATION 
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
---
+++ 01_basics/04_conversions.js
@@ -0,0 +1,17 @@
+// console.log(2>4);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1)
+//COMPARISION WHEN DATATYPES ARE NOT SAME
+// console.log("2">1);
+// console.log("02">1);                     ALL THESE COMPARISONS ARE NOT GOOD . WE SHOULD NOT FOLLOW THESE COMPARISONS
+//COMPARISION OF NULL AND 0
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+//COMPARISION OF UNDEFINED AND 0
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+console.log("2"===2);//this triple equal always gives false
