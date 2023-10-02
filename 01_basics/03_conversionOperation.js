bf90b3d333fa15593865bc1d932fce43d9f88174
Hansraj100 <89459145+Hansraj100@users.noreply.github.com>
2023-10-02 12:48:44 +0000

    Datatype conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+// conversion of datatypes into numbers
+let score = "33"
+// console.log(typeof score);
+// console.log(typeof(score));
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+//"33"=>33
+//"33ABC"=>NaN
+//True=>1,False=>0
+//null=>0 but it is object
+let isLoggedIn = "hansraj"
+let booleanIsLoggedIn = Boolean( isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// conversion of datatypes into boolean
+// 1=>true,0=>false
+// ""=>false,"hansraj"=>true
+let someNumber = 33
+let StringNumber = String(someNumber)
+console.log(StringNumber );
+console.log(typeof StringNumber);
