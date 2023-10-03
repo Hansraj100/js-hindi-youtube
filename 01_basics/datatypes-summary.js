243c37de7aaad8d47b3a0ae7dca2605c782aecc2
Hansraj100 <89459145+Hansraj100@users.noreply.github.com>
2023-10-03 07:11:15 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,22 @@
+//#PRIMITIVE DATATYPE
+//7// String,Number,Boolean,Null,Undefined,Symbol,BigInt
+// let name = "hansraj"//string
+// const  score = 33//number
+// const score1 = 33.5//number
+// const isLoggedIn = true// boolean
+// const outsideTemp = null//null
+// let userEmail;//undefined
+// const id = Symbol('123') 
+// const newId = Symbol('123')
+// console.log(id===newId);
+//#NON PRIMITIVE(REFERENCE)
+//1-ARRAY , 2-OBJECT , 3-FUNCTION
+// const heroesArray = [shaktiman , nagraj , spiderman]//ARRAY
+// let myObject = {
+//     name:"hansraj"
+//     age:23
+// }
+const myFunction = function(){
+    console.log("hello world");
+}
+
