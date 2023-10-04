bd34bff04e21676bb86ff9edceabdc0a79b69fd6
Hansraj100 <89459145+Hansraj100@users.noreply.github.com>
2023-10-04 19:24:52 +0000

    math and num

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,32 @@
+const score = 100
+// console.log(score);
+const balance = new Number(400);
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));//THIS TO FIXED METHOD USED IN E COMMERCE WEBSITES TO SHOW PRECISION TO THE CUSTOMER
+const otherNumber = 123.8659
+// console.log(otherNumber.toPrecision(5));// THIS PRECISION METHOD USED TO ROUND OFF THE NUMBER AND SHOW US WHAT WE INPUT
+const hundreads = 1000000000000
+// console.log(hundreads.toLocaleString('en-IN'));//this will represent the number with the commas and it makes readeability easy by applying commas 
+
+
+                    //maths //    
+
+
+console.log(Math);
+console.log(Math.abs(-3));
+console.log(Math.round(6.897));
+console.log(Math.ceil(6.2));
+console.log(Math.floor(4.2));
+console.log(Math.sqrt(25));
+console.log(Math.min(2,3,4,5));
+console.log(Math.max(2,3,4,5));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.random()*10 + 1);
+ const min = 10
+ const max = 20
+
+console.log(Math.floor(Math.random()*(max - min ) + 1) + min);
+
