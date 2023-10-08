9f186e19714e969485cbff6449cf3339a1f52700
Hansraj100 <89459145+Hansraj100@users.noreply.github.com>
2023-10-08 19:43:58 +0000

    scope part 1

---
+++ 01_basics/02_basics/03_basics/02_scopes.js
@@ -0,0 +1,21 @@
+
+if(true){
+    let a = 10
+const b = 20 
+var c = 30
+}
+// console.log(a);
+// console.log(b);
+//console.log(c);//THIS WILL PRINT 30 IN TERMINAL WHICH IS THE PROBLEM SO WE SHOULD NOT USE VAR
+let a = 500
+var b = 600
+if(true){
+let a = 10
+var b = 50
+console.log("inner: ", b)
+}
+console.log(a);
+console.log(b);
+********THE TYPE OF SCOPE WE ARE GETTING IN WINDOWS IS DIFFERENT FROM CODE ENVIRONMENT SCOPE 
+//we will learn about core  scope in further videos
+
