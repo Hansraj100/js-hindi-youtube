f5feac4089ec6c7b4a24b5017a38de901c2e33ce
Hansraj100 <89459145+Hansraj100@users.noreply.github.com>
2023-10-10 12:31:28 +0000

    if ,else and else if basics

---
+++ 01_basics/02_basics/03_basics/04_controlflow/one.js
@@ -0,0 +1,56 @@
+// if 
+// const isUserLoggedIn = true
+// if(isUserLoggedIn ){
+// }
+// if(2==="2"){
+//     console.log("executed");
+// }
+//OPERATORS
+//<,>,>=,<=,!=,==(this double equal only compare values),===(this triple equal also compare data type ),
+// if(2!==3){
+//     console.log("executed");
+// }
+// const temprature = 55
+// if(temprature<50){
+//     console.log("less than 50");
+// }else {
+//     console.log("temprature is more than 50");
+// }
+
+// const score = 200
+// if(score>100){
+// const  power = "fly"
+// console.log(`user power :${power}`);
+// }
+// console.log(`user power :${power}`) //(it will give value if we use var inside block or scope)
+
+const balance = 759
+//if(balance>50) console.log("test");//(THIS IS IMPLICIT AS IT WILL ASSUME SCOPE AUTOMATICALLY)this code should be in a single  life
+//  if(balance<500){
+//     console.log("less than 500");
+//  }else if(balance<750){
+//     console.log("less than 750");
+//  }else if (balance<900){
+//     console.log("less than 900");
+//  }else if(balance<1000){
+//     console.log("less than 1000");
+//  }else {
+//     console.log("less than 1200");
+//  }
+ const userLoggedIn = true
+ const debitCard = true
+ const isLoggedInFromGoogle = false
+ const isLoggedInFromGmail = true
+ if(userLoggedIn && debitCard && 2==2){
+   console.log("Allow to buy course");
+ }
+ if(isLoggedInFromGoogle || isLoggedInFromGmail ){
+   console.log("user logged in");
+ }
+ 
+
+
+
+
+
+
